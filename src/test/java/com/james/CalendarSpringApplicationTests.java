ebcf89c565eecee477cebee1333982010ca96846
jamesyburr <vint.james@gmail.com>
2016-06-27 10:37:33 -0400

    added tests

--- src/test/java/com/james/CalendarSpringApplicationTests.java
+++ src/test/java/com/james/CalendarSpringApplicationTests.java
@@ -1,18 +1,65 @@
 package com.james;
 
+import com.james.services.EventRepository;
+import com.james.services.UserRepository;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.time.LocalDateTime;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = CalendarSpringApplication.class)
 @WebAppConfiguration
 public class CalendarSpringApplicationTests {
 
+	@Autowired
+	WebApplicationContext wac;
+
+	MockMvc mockMvc;
+
+	@Before
+	public void before() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+	}
+
+	@Autowired
+	UserRepository users;
+
+	@Autowired
+	EventRepository events;
+
 	@Test
-	public void contextLoads() {
+	public void testLogin() throws Exception {
+		mockMvc.perform(
+				MockMvcRequestBuilders.post("/login")
+						.param("username", "name")
+						.param("password", "password")
+		);
+
+		Assert.assertTrue(users.count() == 1);
+	}
+
+	@Test
+	public void testAddEvent() throws Exception {
+		testLogin();
+
+		mockMvc.perform(
+				MockMvcRequestBuilders.post("/create-event")
+				.param("description", "Test event")
+				.param("time", LocalDateTime.now().toString())
+				.sessionAttr("username", "Alice")
+		);
+		Assert.assertTrue(events.count() == 1);
 	}
 
 }
